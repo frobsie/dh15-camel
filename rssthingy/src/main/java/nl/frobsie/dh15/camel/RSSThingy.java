cf1f3df206f5632051c5a9f70e1764b3090866df
Johan van der Graaff <j.van.der.graaff@anva.nl>
2015-03-31 20:19:09 +0200

    Comments & statics

--- rssthingy/src/main/java/nl/frobsie/dh15/camel/RSSThingy.java
+++ rssthingy/src/main/java/nl/frobsie/dh15/camel/RSSThingy.java
@@ -31,6 +31,29 @@ public class RSSThingy {
     public static String SQL_UPDATE_PROCESSED = "update feedentryqueue set processed = true where id = :#id";
     public static String SQL_UPDATE_EXTERNALID = "update feedentryqueue set externalid = :#externalid where id = :#id";
 
+    /** Registry keys */
+    public static String REGISTRY_KEY_DATASOURCE = "jdbc/postgres";
+    public static String REGISTRY_KEY_FEEDBEAN = "feedBean";
+
+    /**
+     * Gebruikte EIP's :
+     * - Pipes and Filters
+     * - Message Translator (beans -> geen afhankelijkheid camel)
+     * - Multicast
+     *
+     * Leest een RSS feed uit, zet de items in een PostgreSQL 
+     * database, voert daar een (simpele) bewerking op uit,
+     * transformeert het resultaat middels XSLT naar HTML
+     * en verstuurt deze per mail richting eindgebruiker.
+     *
+     * De gegevens die middels de SQL Component opgeslagen
+     * worden in de database zijn puur voor testdoeleinden
+     * ingebouwd. Middels een simpel php script kunnen deze
+     * gegevens uitgelezen worden op een website.
+     * 
+     * @param  args[]
+     * @throws Exception
+     */
     public static void main(String args[]) throws Exception {
 
         // Init
@@ -39,8 +62,8 @@ public class RSSThingy {
         FeedBean feedBean = new FeedBean();
 
         // Voeg de custom beans to aan de registry
-        registry.put("jdbc/postgres", postgresBean.getDataSource());
-        registry.put("feedBean", feedBean);
+        registry.put(REGISTRY_KEY_DATASOURCE, postgresBean.getDataSource());
+        registry.put(REGISTRY_KEY_FEEDBEAN, feedBean);
 
         // Bouw de camelContext op met de registry
         CamelContext context = new DefaultCamelContext(registry);
@@ -48,14 +71,8 @@ public class RSSThingy {
         // Voeg de route toe aan de camelContext
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-
-                /*
-                 * Gebruikte EIP's :
-                 * - Pipes and Filters
-                 * - Message Translator (beans -> geen afhankelijkheid camel)
-                 * - Multicast
-                 */
                 
+                // Controleer of gmail gegevens aanwezig zijn
                 RSSThingy.validateGmail();
                 
                 /**
@@ -84,14 +101,14 @@ public class RSSThingy {
                  */
                 from("direct:rssResults").
                     to("bean:feedBean?method=convert").
-                    to("sql:" + SQL_INSERT + "?dataSource=jdbc/postgres");
+                    to("sql:" + SQL_INSERT + "?dataSource=" + REGISTRY_KEY_DATASOURCE);
 
                 /**
                  * Route 4
                  * Verwerkt elk record in de feedentryqueue tabel 
                  * zodat het externalid (a.d.h.v. de link) gevuld wordt.
                  */
-                from("sql:" + SQL_GET_UNPROCESSED + "?dataSource=jdbc/postgres").
+                from("sql:" + SQL_GET_UNPROCESSED + "?dataSource=" + REGISTRY_KEY_DATASOURCE).
                     to("bean:feedBean?method=processEntry").
                     multicast().
                         to("direct:updateProcessed", "direct:updateExternalId");
@@ -101,14 +118,14 @@ public class RSSThingy {
                  * Update processed boolean in database.
                  */
                 from("direct:updateProcessed").
-                    to("sql:" + SQL_UPDATE_PROCESSED + "?dataSource=jdbc/postgres");
+                    to("sql:" + SQL_UPDATE_PROCESSED + "?dataSource=" + REGISTRY_KEY_DATASOURCE);
 
                 /**
                  * Route 6
                  * Update externalid in database.
                  */
                 from("direct:updateExternalId").
-                    to("sql:" + SQL_UPDATE_EXTERNALID + "?dataSource=jdbc/postgres");
+                    to("sql:" + SQL_UPDATE_EXTERNALID + "?dataSource=" + REGISTRY_KEY_DATASOURCE);
 
                 /**
                  * Route 7
